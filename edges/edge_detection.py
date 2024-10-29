a5258aa8c27596764b73e7ace02f40335d65a472
IvonLuiz <ivonluizgarcia@gmail.com>
2024-10-29 00:21:56 -0300

    finding edges images combining Iu and Iv

--- edges/edge_detection.py
+++ edges/edge_detection.py
@@ -10,13 +10,28 @@ def cv2_imshow(img, title=''):
     plt.axis('off')
     plt.show()
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
 
+# Reading image
+dir_path = os.path.dirname(os.path.realpath(__file__))
 image = cv2.imread(dir_path + '/penguins.png')
 cv2_imshow(image, "Original")
 
-# Convert to gray scale
-img_grau = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Check gray scale image
+if len(image.shape) == 2:
+    print("Image in gray scale.")
+else:
+    if np.all(image[:, :, 0] == image[:, :, 1]) and np.all(image[:, :, 1] == image[:, :, 2]):
+        print("Imagem in gray scale (same values for R, G, B).")
+    else:
+        print("Image is colored.")
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+# Apply Gaussian blur
+image_blur = cv2.GaussianBlur(image, (5, 5), 1)
+cv2_imshow(image_blur, "Blur")
+
 
 # Sobel operator
 Kv = np.array(
@@ -27,9 +42,16 @@ Kv = np.array(
     ]
 )
 Ku = Kv.T
-Iu = cv2.filter2D(img_grau, -1, Ku)
-Iv = cv2.filter2D(img_grau, -1, Kv)
+
+# Convolution
+Iu = cv2.filter2D(image_blur, -1, Ku)
+Iv = cv2.filter2D(image_blur, -1, Kv)
 
 cv2_imshow(Iu, "Iu")
 cv2_imshow(Iv, "Iv")
-# img_sobel = cv2.filter2D(img_grau, -1, KV)
+
+# Combining
+I = np.sqrt(np.square(Iu) + np.square(Iv))
+I = (I/I.max()*255).astype('uint8')
+
+cv2_imshow(I, "I")
