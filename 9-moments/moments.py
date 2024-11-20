c013a451dff5cf85c40763d9dd261e634482bd79
IvonLuiz <ivonluizgarcia@gmail.com>
2024-11-20 19:51:52 -0300

    ellipse calc

--- 9-moments/moments.py
+++ 9-moments/moments.py
@@ -2,6 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+import random as rng
 
 def imshow(img):
     plt.figure(figsize=(8, 6), dpi=80)
@@ -15,23 +16,23 @@ img_path = os.path.join(dir_path, "aviao_ed.png")
 img = cv2.imread(img_path)
 
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-_, binary_img = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY)
+_, binary_img = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
 
-# imshow(binary_img)
+imshow(binary_img)
+
 # Image moments
-M = cv2.moments(gray_image)
+M = cv2.moments(binary_img)
 
 # a) Area
 area = M['m00']
 print(f"Area from object: {area}")
 
 # b) Centroids
-cX = int(M["m10"] / area)
-cY = int(M["m01"] / area)
+cX = M["m10"] / area
+cY = M["m01"] / area
 centroids = (int(cX), int(cY))
-cv2.circle(img, centroids, 5, (255, 0, 0), -1)
-cv2.putText(img, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
+cv2.circle(img, centroids, 5, (255, 0, 0), -1)
 imshow(img)
 
 # c) Central moments and inertia matrix
@@ -40,5 +41,15 @@ mu02 = M["mu02"]
 mu11 = M["mu11"]
 
 inertia_matrix = np.array([[mu20, mu11], [mu11, mu02]])
-print(inertia_matrix)
+print(f"Inertia matrix: \n {inertia_matrix}")
+
+# d) Equivalent ellipse
+eigenvalues, eigenvectors = np.linalg.eig(inertia_matrix)
+
+a = 4 * np.sqrt(eigenvalues[0] / area)  # major axis length
+b = 4 * np.sqrt(eigenvalues[1] / area)  # minor axis length
+
+axes = (int(a / 2), int(b / 2))
+
+print(f"Major axis 'a': {a}\nMinor axis 'b': {b}")
 
